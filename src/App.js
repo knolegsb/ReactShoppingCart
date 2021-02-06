46ed037be894bce57ea930abab750080d670147e
knolegsb <seon2you@hotmail.com>
2021-02-06 09:57:34 -0800

    added size filter and order by price

--- src/App.js
+++ src/App.js
@@ -13,6 +13,34 @@ class App extends React.Component {
     }
   }
 
+  sortProducts = (event) => {
+    console.log(event.target.value);
+    const sort = event.target.value;
+
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products.slice()
+        .sort((a, b) =>
+          sort === "lowest" ? a.price > b.price ? 1 : -1 
+                    : sort === "highest" ? a.price < b.price ? 1 : -1
+                                : a._id < b._id ? 1 : -1
+      ),
+    }));
+  };
+
+  filterProducts = (event) => {
+    console.log(event.target.value);
+    if(event.target.value === "") {
+      this.setState({size:event.target.value, products: data.products});
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        )
+      });
+    }
+  };
   render() {
     return (
       <div className="grid-container">
@@ -22,7 +50,13 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
-              <Filter></Filter>
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProduct={this.filterProducts}
+                sortProduct={this.sortProducts}
+              ></Filter>
               <Products products={this.state.products}></Products>
             </div>
             <div className="sidebar">Cart Items</div>
