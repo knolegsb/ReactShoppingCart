fc699b31c01f8fcb2e500b7608b2c71e071161c6
knolegsb <seon2you@hotmail.com>
2021-02-06 14:42:23 -0800

    added functionality and fixed some bugs

--- src/App.js
+++ src/App.js
@@ -9,10 +9,34 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: "",
       sort: ""
     }
   }
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+
+    if (!alreadyInCart) {
+      cartItems.push({...product, count: 1});
+    }
+    this.setState({cartItems});
+  }
+
+  removeFromCart = (product) => {
+    const cartItmes = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItmes.filter((x) => x._id !== product._id)
+    });
+  };
 
   sortProducts = (event) => {
     console.log(event.target.value);
@@ -58,10 +82,10 @@ class App extends React.Component {
                 filterProduct={this.filterProducts}
                 sortProduct={this.sortProducts}
               ></Filter>
-              <Products products={this.state.products}></Products>
+              <Products products={this.state.products} addToCart={this.addToCart}></Products>
             </div>
             <div className="sidebar">
-              <Cart />                
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />                
             </div>
           </div>
         </main>
